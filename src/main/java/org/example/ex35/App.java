369d52558bc98aaaabb992ead9391a6c6b56d47c
Reese Spector <89818858+itsSPECTOR@users.noreply.github.com>
2021-09-27 20:16:36 -0400

    Update App.java

--- src/main/java/org/example/ex35/App.java
+++ src/main/java/org/example/ex35/App.java
@@ -3,6 +3,14 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
+
+/**
+ *
+ * UCF COP3330 Fall 2021 Assignment 35 Solution
+ * Copyright 2021 Reese Spector
+ *
+ */
+
 public class App {
 
     public static void main(String[] args) {
