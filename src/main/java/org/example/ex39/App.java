759170f9afe1d44c9dcacd16e84c66c2722708ca
Reese Spector <89818858+itsSPECTOR@users.noreply.github.com>
2021-09-27 20:15:53 -0400

    Update App.java

--- src/main/java/org/example/ex39/App.java
+++ src/main/java/org/example/ex39/App.java
@@ -1,6 +1,13 @@
 package org.example.ex39;
 import java.util.*;
 
+/**
+ *
+ * UCF COP3330 Fall 2021 Assignment 39 Solution
+ * Copyright 2021 Reese Spector
+ *
+ */
+
 public class App {
 
     public static void main(String[] args) {
